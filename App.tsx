2748c8f2c956b60e6a56800d6629802d5b611f77
Emily Raventos <rav18001@byui.edu>
2024-12-13 15:25:56 -0700

    update before view progress

--- App.tsx
+++ App.tsx
@@ -7,7 +7,6 @@ import TrackProgressScreen from './screens/TrackProgressScreen';
 import ViewProgressScreen from './screens/ViewProgressScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import AuthScreen from './screens/AuthScreen';
-import Icon from 'react-native-vector-icons/Ionicons';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { initDB } from './db';
 import React, { useEffect } from 'react';
@@ -37,54 +36,6 @@ const App: React.FC = () => {
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <NavigationContainer>
-          {/* <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarActiveTintColor: 'tomato',
-              tabBarInactiveTintColor: 'gray',
-              headerShown: false,
-              tabBarStyle: {
-                display: route.name === 'AuthScreen' || route.name === 'Profile' ? 'none' : 'flex', // Hide tab bar on AuthScreen and Profile
-              },
-            })}
-          >
-            <Tab.Screen
-              name="Home"
-              component={HabitStack} // This uses the HabitStack as a component
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <Icon name="home-outline" color={color} size={size} />
-                ),
-                headerShown: false, // Prevent the header from showing here as it's handled by the stack
-              }}
-            />
-            <Tab.Screen
-              name="Create Habit"
-              component={HabitCreationScreen}
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <Icon name="add-circle-outline" color={color} size={size} />
-                ),
-              }}
-            />
-            <Tab.Screen
-              name="Track Progress"
-              component={TrackProgressScreen}
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <Icon name="clipboard" color={color} size={size} />
-                ),
-              }}
-            />
-            <Tab.Screen
-              name="View Progress"
-              component={ViewProgressScreen}
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <Icon name="calendar" color={color} size={size} />
-                ),
-              }}
-            />
-          </Tab.Navigator> */}
           <HabitStack/>
         </NavigationContainer>
       </SafeAreaView>
